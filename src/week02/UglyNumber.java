07eb60bbf0efad6a12c8b76d9a45699b0ed8da2a
bmwyan <270995339@qq.com>
2021-04-11 21:53:20 +0800

    week02

--- src/week02/UglyNumber.java
+++ src/week02/UglyNumber.java
@@ -12,28 +12,28 @@ public class UglyNumber {
 
     public int nthUglyNumber(int n) {
 
-        int[] dp = new int[n];
+        int[] dp = new int[n + 1];
 
-        dp[0]=1;
-        int a=0;
-        int b=0;
-        int c=0;
-        for(int i=1;i<n;i++){
-            int n2=a*2;
-            int n3=b*3;
-            int n5=c*5;
-            dp[i] = Math.min(Math.min(n2,n3),n5);
-            if(dp[i]==n2){
+        dp[0] = 1;
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        for (int i = 1; i <= n; i++) {
+            int n2 = dp[a] * 2;
+            int n3 = dp[b] * 3;
+            int n5 = dp[c] * 5;
+            dp[i] = Math.min(Math.min(n2, n3), n5);
+            if (dp[i] == n2) {
                 a++;
             }
-            if(dp[i]==n3){
+            if (dp[i] == n3) {
                 b++;
             }
-            if(dp[i]==n5){
-               c++;
+            if (dp[i] == n5) {
+                c++;
             }
 
         }
-        return dp[n-1];
+        return dp[n];
     }
 }
