64598bb8bb7935de563d9392d1f63cb4a522eb2d
bmwyan <270995339@qq.com>
2021-04-11 22:14:56 +0800

    week02

--- src/week02/UglyNumber.java
+++ src/week02/UglyNumber.java
@@ -1,19 +1,29 @@
 package week02;
 
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+
 /**
  * @author : admin
  * @version V1.0
  * @Project: leecode
  * @Package week02
- * @Description: TODO
+ * @Description: leecode264
  * @date Date : 2021年04月10日 18:49
  */
 public class UglyNumber {
 
+    /**
+     * 动态规划做法
+     *
+     * @param n
+     * @return
+     */
     public int nthUglyNumber(int n) {
         int[] dp = new int[n + 1];
         dp[0] = 1;
-        int a = 1,b=1,c=1;
+        int a = 1, b = 1, c = 1;
 
         for (int i = 1; i <= n; i++) {
             int n2 = dp[a] * 2;
@@ -33,4 +43,32 @@ public class UglyNumber {
         }
         return dp[n];
     }
+
+    /**
+     * 优先级队列做法
+     *
+     * @param n
+     * @return
+     */
+    public int nthUglyNumber2(int n) {
+        int[] factors = new int[]{2, 3, 5};
+        int uglyNum = 0;
+        Set<Long> set = new HashSet<>();
+        PriorityQueue<Long> queue = new PriorityQueue<>();
+        queue.add(1L);
+        set.add(1L);
+        for (int i = 0; i < n; i++) {
+            long cur = queue.poll();
+            uglyNum = (int) cur;
+            for (int factor : factors) {
+                long nextNum = cur * factor;
+                if (!set.contains(nextNum)) {
+                    queue.add(nextNum);
+                    set.add(nextNum);
+                }
+            }
+        }
+        return uglyNum;
+    }
+
 }
