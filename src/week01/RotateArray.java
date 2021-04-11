de9bf5e15609a3972f18ec049b0be0bc36db8199
bmwyan <270995339@qq.com>
2021-04-11 23:38:17 +0800

    week02

--- src/week01/RotateArray.java
+++ src/week01/RotateArray.java
@@ -40,15 +40,45 @@ public class RotateArray {
         int count = 0;
         for (int start = 0; count < length; start++) {
             int cur = start;
-            int pre =nums[cur];
+            int pre = nums[cur];
             do {
                 count++;
                 int next = (cur + k) % length;
                 int temp = nums[next];
                 nums[next] = pre;
-                pre=temp;
-                cur=next;
+                pre = temp;
+                cur = next;
             } while (start != cur);
         }
     }
+
+
+    /**
+     * 高效翻转效率
+     * 1.先进行整个数组翻转
+     * 2.0~k-1:翻转    k~n-1翻转
+     *
+     * @param nums
+     * @param k
+     */
+    public void rotate3(int[] nums, int k) {
+        int length = nums.length;
+        k = k % length;
+        reverse(nums, 0, length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, length - 1);
+
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+
 }
